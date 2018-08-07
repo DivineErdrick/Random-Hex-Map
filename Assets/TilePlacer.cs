064c9f275ab1be211d309a3d5a6a75f6bdc5fb46
DivineErdrick <40323567+DivineErdrick@users.noreply.github.com>
2018-08-07 17:33:14 -0500

    Tile Placer is slightly more efficient.

--- Assets/TilePlacer.cs
+++ Assets/TilePlacer.cs
@@ -19,6 +19,7 @@ public class TilePlacer : MonoBehaviour {
     bool runGenerator = false;
     int columns;
     int rows;
+    int mapSize;
 
     void Start () {
 
@@ -37,18 +38,36 @@ public class TilePlacer : MonoBehaviour {
 
             HexTile[] hexTiles = FindObjectsOfType<HexTile>();
 
-            if (hexTiles.Length < columns * rows) {
+            if (hexTiles.Length < mapSize) {
                 int randColumn = Random.Range(0, columns);
                 int randRow = Random.Range(0, rows);
                 Debug.Log("Attempting to place hex tile at Column " + randColumn + ", Row " + randRow);
 
+                int columnCount = 0;
+                int rowCount = 0;
                 bool tilePlacedThere = false;
                 foreach (HexTile hexTile in hexTiles) {
+                    if (hexTile.tileColumn == columns - 1) {
+                        columnCount++;
+                    }
+                    if (hexTile.tileRow == rowCount - 1) {
+                        rowCount++;
+                    }
 
                     if (hexTile.tileColumn == randColumn && hexTile.tileRow == randRow) {
                         tilePlacedThere = true;
                     }
                 }
+
+                if (columnCount == rows && rowCount == columns) {
+                    columns--;
+                    rows--;
+                } else if (columnCount == rows) {
+                    columns--;
+                } else if (rowCount == columns) {
+                    rows--;
+                }
+
                 if ( ! tilePlacedThere) {
 
                     GameObject newTile = Instantiate(availableTiles[0]);
@@ -80,6 +99,7 @@ public class TilePlacer : MonoBehaviour {
 
             columns = tilePlacerUI.Columns;
             rows = tilePlacerUI.Rows;
+            mapSize = columns * rows;
 
             HexTile[] hexTiles = FindObjectsOfType<HexTile>();
             foreach (HexTile hexTile in hexTiles) {
